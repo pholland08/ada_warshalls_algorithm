b8f98948c60c9eaade6cc7f870e50b6e4276c9f2
pholland08 <bulldog105@gmail.com>
2017-02-03 22:30:13 -0600

    First commit
    Some basic files to get things started.

---
+++ src/arrays.ads
@@ -0,0 +1,8 @@
+--This is where the array types are initialized for use in other files
+
+package arrays is
+
+   type intArray is array (Integer range <>) of Integer;
+   type charArray is array (Integer range <>) of Character;
+
+end arrays;
