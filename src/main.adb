16a453ed4151d536c123cec7d29da586a5659c92
pholland08 <bulldog105@gmail.com>
2017-02-05 00:17:07 -0600

    Begin outline for main file
    Make a copy of arrays.adb (but it'll probably be deleted).
    Start building structure for main.adb

--- src/main.adb
+++ src/main.adb
@@ -1,32 +1,18 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
---with & use the arrays.ads package
-with arrays; use arrays;
+--include dependencies
 
 
 procedure Main is
 
- --array types declared in arrays.ads
- intArr : intArray(1..3) := (0,1,2);
- charArr : charArray(0..2) := ('a','b','c');
+   --declare variables
 
 begin
+   --prompt user for input
+     --convert to desired data type
+   --allocate memory for array based on input
+   --print each transaction as it's processed
 
-   --if declaring a new variable after beginning the procedure, you have to
-     --use a declare statement and another begin statement
-   declare
-
-      charIndex : Integer;
-   begin
-      -- Use 'range to get the range of an array
-      for I in intArr'Range loop
-         charIndex:=intArr(i);
-         --Ada deals with char and int in a weird way so to print
-           --them you have to use 'image
-         Put_Line (charArr(charIndex)'image);
-
-      end loop;
-      end;
 
 end Main;
 
