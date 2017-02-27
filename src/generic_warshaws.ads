e8aee882f1c9e91a1645af987afd28fd61fc8115
pholland08 <bulldog105@gmail.com>
2017-02-27 01:25:52 -0600

    Significant changes
    Abstracted and cleaned primary files. A-option complete, barring
    unforseen issues.

--- src/generic_warshaws.ads
+++ src/generic_warshaws.ads
@@ -1,25 +1,19 @@
-
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 
 generic
-   type data_type is private;
-   type boolean_matrix is array
-     (Positive range <>, Positive range <>) of data_type;
-   type string_array is array (Positive range <>)
-     of Ada.Strings.Unbounded.Unbounded_String;
-   with function "or"(left, right: data_type) return data_type;
-   with procedure put(x: data_type);
-   input_path, output_path : String;
-
+   type my_type is (<>);
+   default, affirmative : my_type;
+   with procedure Put (file_name: File_Type; T : my_type);
+   with function "or" (left, right : my_type) return my_type;
 
 package generic_warshaws is
-   node_count : integer;
-   input_file, output_file : File_Type;
-
-   procedure run_it;
-   procedure warshawsAlgorithm (bmr : boolean_matrix);
+   type boolean_matrix is array(Positive range <>,
+                                Positive range <>) of my_type;
+   procedure run(input_path, output_path : String);
+   procedure warshaws_algorithm (bmr : in out boolean_matrix;
+                                 affirmative : in my_type);
 
 end generic_warshaws;
--- src/main.adb
+++ src/main.adb
@@ -1,24 +1,84 @@
 
+--------------------------------------------------------------------------------
+----------------------------- Main procedure------------------------------------
+--------------------------------------------------------------------------------
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
-
-with MyPac; use MyPac;
-with warshawsAlgorithm;-- use warshawsAlgorithm;
---include dependencies
-
+with generic_warshaws;
 
 procedure Main is
-   package SU renames Ada.Strings.Unbounded;
 
-   inputFile, outputFile : File_Type;
-   -- Write nextLine function
-   inputLine, outputLine : SU.Unbounded_String;
-   Node_Count : Integer;
+-------------------------- Boolean package overloads----------------------------
+   ---- IO procedure to pass for booleans
+   procedure boolean_put(out_file : File_Type; x : Boolean) is
+      output : integer := 0;
+   begin
+      if x then
+         output := 1;
+      end if;
+      Put(out_file, output'Image&"   ");
+   end;
 
+   ---- "or" operator overload for booleans
+   function boolean_or(left, right: Boolean) return Boolean is
+   begin
+      if right then
+         return right;
+      else
+         return left;
+      end if;
+   end;
+
+-------------------------- Integer package overloads----------------------------
+   ---- IO procedure to pass for integers
+   procedure integer_put(out_file : File_Type; x : Integer) is
+   begin
+      Ada.Text_IO.Put(out_file, x'Image&"   ");
+   end;
+
+   ---- "or" operator overload for integers
+   function integer_or(left, right: Integer) return Integer is
+   begin
+      if Left+Right > 0 then
+         return 1;
+      else
+         return 0;
+      end if;
+   end;
+
+---------------------- Boolean package initialization---------------------------
+   package boolean_war_pac is new generic_warshaws(my_type => Boolean,
+                                                   default => False,
+                                                   affirmative => True,
+                                                   Put     => boolean_put,
+                                                   "or"    => boolean_or);
+   use boolean_war_pac;
+
+--------------------- Integer package initialization----------------------------
+   package integer_war_pac is new generic_warshaws(my_type => Integer,
+                                                   default => 0,
+                                                   affirmative => 1,
+                                                   Put     => integer_put,
+                                                   "or"    => integer_or);
+   use integer_war_pac;
+
+--------------------------------------------------------------------------------
+--------------------------- Begin main procedure--------------------------------
+--------------------------------------------------------------------------------
 begin
+   ---- Run each input file and output the results
+   boolean_war_pac.run(input_path => "C_Input.txt",
+                       output_path => "C_Output.txt");
 
+   integer_war_pac.run(input_path => "B_Input.txt",
+                       output_path => "B_Output.txt");
+
+   integer_war_pac.run(input_path => "A1_Input.txt",
+                       output_path => "A1_Output.txt");
+
+   integer_war_pac.run(input_path => "A2_Input.txt",
+                       output_path => "A2_Output.txt");
+
+--------------------------- End main procedure----------------------------------
 end Main;
 
 
